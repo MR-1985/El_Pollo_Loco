b809fcb4b4f120e42ab428ee99c72fab2c70ebb9
Marco Rößler <marco_r1985@yahoo.de>
2026-06-04 22:26:03 +0200

    created classes;

---
+++ models/movable-objects.class.js
@@ -0,0 +1,15 @@
+class MovableObject {
+    x = 120;
+    y = 400;
+    img;
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+    moveRight() {
+        console.log('move right');
+    }
+
+}
